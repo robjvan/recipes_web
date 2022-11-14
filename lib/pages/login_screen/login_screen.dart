000e277ad2f07df75693eb396cf835c06fe8348a
Rob Vandelinder <robjvan@gmail.com>
2022-11-14 15:29:38 -0330

    Added LoginScreen placeholder

---
+++ lib/pages/login_screen/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+  static String routeName = '/login';
+
+  @override
+  Widget build(final BuildContext context) {
+    return Container();
+  }
+}
