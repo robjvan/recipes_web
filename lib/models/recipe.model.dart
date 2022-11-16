d0a847b5f816d1769ba2a9c60b4a24dfbce6ac5f
Rob Vandelinder <robjvan@gmail.com>
2022-11-15 21:31:27 -0330

    Created basic Recipe model

---
+++ lib/models/recipe.model.dart
@@ -0,0 +1,25 @@
+class Recipe {
+  int id;
+  String title;
+  int userId;
+
+  //TODO: Add missing fields
+
+  Recipe({
+    required this.id,
+    required this.title,
+    required this.userId,
+  });
+
+  factory Recipe.fromJson(Map<String, dynamic> json) => Recipe(
+        id: json["id"],
+        title: json["title"],
+        userId: json["userId"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "userId": userId,
+      };
+}
