6b677e24a754f52a83986ba6eeb02b307232205c
Rob Vandelinder <robjvan@gmail.com>
2022-11-16 14:19:22 -0330

    Created placeholder file

---
+++ lib/pages/signup_screen/signup_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// TODO: Build signup/register screen
