36b2819ad9f2adc0bc028ceffb731c3947e34f1b
Rob Vandelinder <robjvan@gmail.com>
2022-11-16 14:17:23 -0330

    Refactored widget tree

--- lib/pages/login_screen/login_screen.dart
+++ lib/pages/login_screen/login_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:recipes_web/api/recipes_api.dart';
+import 'package:recipes_web/api/auth_api.dart';
 import 'package:recipes_web/config/config.dart';
 import 'package:recipes_web/controllers/api_state.controller.dart';
 import 'package:recipes_web/models/dto/sign_in.dto.dart';
-import 'package:recipes_web/providers/recipes_api_provider.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -13,83 +12,97 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    final TextEditingController usernameController = TextEditingController();
-    final TextEditingController passwordController = TextEditingController();
-    final RecipesAPI recipesAPI = RecipesAPI(RecipesAPIProvider());
     final double sw = MediaQuery.of(context).size.width;
     final ApiStateController apiState = Get.put(ApiStateController());
 
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(color: Theme.of(context).splashColor),
+        decoration: BoxDecoration(color: Colors.blue[50]),
         width: sw,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Recipes',
-              style: TextStyle(
-                fontSize: 32,
-                color: Theme.of(context).primaryColor,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            _buildHeaderWidget(context),
             const SizedBox(height: 32),
             Obx(
               () => apiState.isLoading.value
-                  ? const CircularProgressIndicator() // TODO(Rob): Add some style
-                  : Form(
-                      key: formKey,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            TextFormField(
-                              controller: usernameController,
-                              validator: (final String? value) {
-                                if (value!.isEmpty) {
-                                  return 'Username cannot be empty';
-                                }
-                                return null;
-                              },
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: const InputDecoration(),
-                            ),
-                            TextFormField(
-                              controller: passwordController,
-                              validator: (final String? value) {
-                                if (value!.isEmpty) {
-                                  return 'Password cannot be empty';
-                                }
-                                return null;
-                              },
-                              obscureText: true,
-                              decoration: const InputDecoration(),
-                            ),
-                            ElevatedButton(
-                              child: const Text('Login'),
-                              onPressed: () async {
-                                if (formKey.currentState!.validate()) {
-                                  await recipesAPI.signIn(
-                                    signInCredentials: SignInDto(
-                                      username: usernameController.text,
-                                      password: passwordController.text,
-                                      platform: await Config().getPlatform(),
-                                    ),
-                                  );
-                                }
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                  ? _buildProgressIndicator()
+                  : _buildLoginForm(),
             ),
           ],
         ),
       ),
     );
   }
+
+  Widget _buildHeaderWidget(final BuildContext context) => Text(
+        'Recipes',
+        style: TextStyle(
+          fontSize: 32,
+          color: Theme.of(context).primaryColor,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+
+  Widget _buildLoginForm() {
+    final TextEditingController usernameController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    final AuthAPI authAPI = AuthAPI();
+
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // Username (email address) field
+            TextFormField(
+              controller: usernameController,
+              validator: (final String? value) {
+                if (value!.isEmpty) {
+                  return 'Username cannot be empty';
+                }
+                return null;
+              },
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(),
+            ),
+            // Password Field
+            TextFormField(
+              controller: passwordController,
+              validator: (final String? value) {
+                if (value!.isEmpty) {
+                  return 'Password cannot be empty';
+                }
+                return null;
+              },
+              obscureText: true,
+              decoration: const InputDecoration(),
+            ),
+            /// Login button
+            ElevatedButton(
+              child: const Text('Login'),
+              onPressed: () async {
+                if (formKey.currentState!.validate()) {
+                  await authAPI.signIn(
+                    signInCredentials: SignInDto(
+                      username: usernameController.text,
+                      password: passwordController.text,
+                      platform: await Config().getPlatform(),
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProgressIndicator() => const CircularProgressIndicator(
+      // TODO(Rob): Add some styling to the indicator
+      );
 }
