0f0fe03df321e32966472681763e42d31bd34891
Rob Vandelinder <robjvan@gmail.com>
2022-11-16 23:07:38 -0330

    Renamed local vars

--- lib/api/auth_api.dart
+++ lib/api/auth_api.dart
@@ -6,14 +6,16 @@ import 'package:http/http.dart' as http;
 import 'package:recipes_web/controllers/api_state.controller.dart';
 import 'package:recipes_web/controllers/auth.controller.dart';
 import 'package:recipes_web/models/access_token.dart';
-import 'package:recipes_web/models/dto/sign_in.dto.dart';
+import 'package:recipes_web/pages/login_screen/dto/sign_in.dto.dart';
 import 'package:recipes_web/pages/dashboard_screen/dashboard_screen.dart';
+import 'package:recipes_web/pages/signup_screen/dto/sign-up.dto.dart';
+import 'package:recipes_web/providers/auth_api_provider.dart';
 import 'package:recipes_web/providers/recipes_api_provider.dart';
 
 class AuthAPI {
-  final RecipesAPIProvider _recipesAPIProvider = RecipesAPIProvider();
-  final AuthController authController = Get.put(AuthController());
-  final ApiStateController apiState = Get.put(ApiStateController());
+  final AuthAPIProvider _authAPIProvider = AuthAPIProvider();
+  final AuthController _authController = Get.put(AuthController());
+  final ApiStateController _apiState = Get.put(ApiStateController());
 
   Future<void> _buildErrorDialog(final String message) => Get.defaultDialog(
         title: 'Error',
@@ -22,20 +24,19 @@ class AuthAPI {
       );
 
   /// Sign in an existing user
-  Future<void> signIn(
-      {required final SignInDto signInCredentials}) async {
-    apiState.setLoadingState(true);
+  Future<void> signIn(final SignInDto signInCredentials) async {
+    _apiState.setLoadingState(true);
     late http.Response response;
 
     try {
-      response = await _recipesAPIProvider.signIn(signInCredentials);
+      response = await _authAPIProvider.signIn(signInCredentials);
 
       if (response.statusCode == 201) {
         final AuthToken json = AuthToken.fromJson(jsonDecode(response.body));
 
         // Parse and store authToken
         final String parsedToken = json.token;
-        authController.saveToken(parsedToken);
+        _authController.saveToken(parsedToken);
 
         /// Navigate to Dashboard
         await Get.off(DashboardScreen.new);
@@ -43,20 +44,27 @@ class AuthAPI {
         await _buildErrorDialog(
           'Could not connect to server, please try again later',
         );
-      } else if (response.statusCode == 500) {        
+      } else if (response.statusCode == 500) {
         await _buildErrorDialog('Server error, please try again later');
       } else if (response.statusCode == 400 || response.statusCode == 401) {
         await _buildErrorDialog(
           'Invalid username or password, please try again',
         );
       }
-      apiState.setLoadingState(false);
+      _apiState.setLoadingState(false);
     } on Exception catch (err) {
       await _buildErrorDialog(
         'Could not connect to server, please try again later',
       );
       debugPrint(err.toString());
-      apiState.setLoadingState(false);
+      _apiState.setLoadingState(false);
     }
+  }
+
+  Future<dynamic> signUp(final SignUpDto signupCredentials) async {
+    _apiState.setLoadingState(true);
+    late http.Response response;
+
+    
   }
 }
