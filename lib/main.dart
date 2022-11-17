6307d0b96b85776c7f15183f43cd4cf9e00ec5a2
Rob Vandelinder <robjvan@gmail.com>
2022-11-16 23:07:14 -0330

    Swapped routes for GetPages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_jailbreak_detection/flutter_jailbreak_detection.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:platform_plus/platform_plus.dart';
 import 'package:recipes_web/pages/dashboard_screen/dashboard_screen.dart';
@@ -11,6 +12,7 @@ import 'package:recipes_web/pages/login_screen/login_screen.dart';
 import 'package:recipes_web/pages/recipes_screen/recipes_screen.dart';
 import 'package:recipes_web/pages/settings_screen/settings_screen.dart';
 import 'package:recipes_web/pages/shoppinglist_screen/shoppinglist_screen.dart';
+import 'package:recipes_web/utils/translations.dart';
 
 // Store<AppState> globalStore;
 
@@ -41,7 +43,9 @@ class MyApp extends StatelessWidget {
   // ignore: prefer_expression_function_bodies
   Widget build(final BuildContext context) {
     return GetMaterialApp(
+      locale: Locale('en', 'US'),
       title: 'App Title',
+      translations: MyTranslations(), 
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -49,20 +53,28 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // initialRoute: LoadingScreen.routeName, //TODO: Add load logic
       initialRoute: LoginScreen.routeName,
-      
-      // getPages: getPages,
-      routes: <String, WidgetBuilder>{
-        DashboardScreen.routeName: (final BuildContext ctx) =>
-            const DashboardScreen(),
-        LoginScreen.routeName: (final BuildContext ctx) => const LoginScreen(),
-        RecipesScreen.routeName: (final BuildContext ctx) =>
-            const RecipesScreen(),
-        SettingsScreen.routeName: (final BuildContext ctx) =>
-            const SettingsScreen(),
-        ShoppingListScreen.routeName: (final BuildContext ctx) =>
-            const ShoppingListScreen(),
-        // LoadingScreen.routeName: (ctx) => const LoadingScreen(),
-      },
+      getPages: [
+        GetPage<dynamic>(
+          name: LoginScreen.routeName,
+          page: () => const LoginScreen(),
+        ),
+        GetPage<dynamic>(
+          name: DashboardScreen.routeName,
+          page: () => const DashboardScreen(),
+        ),
+        GetPage<dynamic>(
+          name: RecipesScreen.routeName,
+          page: () => const RecipesScreen(),
+        ),
+        GetPage<dynamic>(
+          name: SettingsScreen.routeName,
+          page: () => const SettingsScreen(),
+        ),
+        GetPage<dynamic>(
+          name: ShoppingListScreen.routeName,
+          page: () => const ShoppingListScreen(),
+        ),
+      ],
     );
   }
 }
