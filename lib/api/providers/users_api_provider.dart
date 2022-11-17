9a1bb37583553d5be63f2e8d2de747b684294df6
Rob Vandelinder <robjvan@gmail.com>
2022-11-17 10:22:38 -0330

    Moved files

---
+++ lib/api/providers/users_api_provider.dart
@@ -0,0 +1,8 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:recipes_web/config/config.dart' as config;
+
+class UsersAPIProvider {
+  final String _apiUrl = '/api/v1';
+}
