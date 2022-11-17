72d775cc501c7a94b34969abb2daf5270e56f4cc
Rob Vandelinder <robjvan@gmail.com>
2022-11-16 23:08:22 -0330

    Created dtos

---
+++ lib/pages/signup_screen/dto/sign-up.dto.dart
@@ -0,0 +1,11 @@
+class SignUpDto {
+  String username;
+  String password;
+  String platform;
+
+  SignUpDto({
+    required this.username,
+    required this.password,
+    required this.platform,
+  });
+}
