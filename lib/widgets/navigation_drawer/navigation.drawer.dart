d00c609f9909efb5e50a61a9f671b6808e793531
Rob Vandelinder <robjvan@gmail.com>
2022-11-17 10:24:31 -0330

    Moved NavigationRow code back to Drawer file

--- lib/widgets/navigation_drawer/navigation.drawer.dart
+++ lib/widgets/navigation_drawer/navigation.drawer.dart
@@ -5,7 +5,7 @@ import 'package:recipes_web/pages/login_screen/login_screen.dart';
 import 'package:recipes_web/pages/recipes_screen/recipes_screen.dart';
 import 'package:recipes_web/pages/settings_screen/settings_screen.dart';
 import 'package:recipes_web/pages/shoppinglist_screen/shoppinglist_screen.dart';
-import 'package:recipes_web/widgets/navigation_drawer/widgets/navigation.row.dart';
+// import 'package:recipes_web/widgets/navigation_drawer/widgets/navigation.row.dart';
 
 class NavigationDrawer extends Drawer {
   const NavigationDrawer({super.key});
@@ -17,36 +17,64 @@ class NavigationDrawer extends Drawer {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: <Widget>[
-                NavigationRow(
+                _buildNavigationRow(
+                  context,
                   navRoute: DashboardScreen.routeName,
                   iconData: Icons.menu,
                   label: 'Dashboard',
                 ),
-                NavigationRow(
+                _buildNavigationRow(
+                  context,
                   navRoute: RecipesScreen.routeName,
                   iconData: Icons.cookie_outlined,
                   label: 'Recipes',
                 ),
-                NavigationRow(
+                _buildNavigationRow(
+                  context,
                   navRoute: ShoppingListScreen.routeName,
                   iconData: Icons.shopping_bag_outlined,
                   label: 'Shopping List',
                 ),
                 Expanded(child: Container()),
-                NavigationRow(
+                _buildNavigationRow(
+                  context,
                   navRoute: SettingsScreen.routeName,
                   iconData: Icons.settings_outlined,
                   label: 'Settings',
                 ),
-                buildLogoutButton(context),
+                _buildLogoutButton(context),
               ],
             ),
           ),
         ),
       );
-}
 
-Widget buildLogoutButton(final BuildContext context) => TextButton(
-      onPressed: () => Get.offAll(LoginScreen.new),
-      child: const Text('Logout'),
-    );
+  Widget _buildLogoutButton(final BuildContext context) => TextButton(
+        onPressed: () => Get.offAll(LoginScreen.new),
+        child: const Text('Logout'),
+      );
+
+  Widget _buildNavigationRow(
+    final BuildContext context, {
+    required final String navRoute,
+    required final IconData iconData,
+    required final String label,
+  }) =>
+      TextButton(
+        onPressed: () => Navigator.pushNamed(
+          context,
+          navRoute,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Row(
+            children: <Widget>[
+              Icon(iconData),
+              const SizedBox(width: 16),
+              Text(label),
+              const Spacer(),
+            ],
+          ),
+        ),
+      );
+}
