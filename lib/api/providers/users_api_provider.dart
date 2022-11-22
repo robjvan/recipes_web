152c820ebbc6849a87d6948d61b864b653ac09bc
Rob Vandelinder <robjvan@gmail.com>
2022-11-21 21:16:02 -0330

    Commented unused code

--- lib/api/providers/users_api_provider.dart
+++ lib/api/providers/users_api_provider.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:http/http.dart' as http;
-import 'package:recipes_web/config/config.dart' as config;
+// import 'package:http/http.dart' as http;
+// import 'package:recipes_web/config/config.dart' as config;
 
 class UsersAPIProvider {
-  final String _apiUrl = '/api/v1';
+  // final String _apiUrl = '/api/v1';
 }
--- lib/pages/signup_screen/signup_screen.dart
+++ lib/pages/signup_screen/signup_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:recipes_web/api/auth_api.dart';
-import 'package:recipes_web/api/users_api.dart';
+// import 'package:recipes_web/api/users_api.dart';
 import 'package:recipes_web/config/config.dart';
-import 'package:recipes_web/controllers/api_state.controller.dart';
+// import 'package:recipes_web/controllers/api_state.controller.dart';
 import 'package:recipes_web/pages/login_screen/login_screen.dart';
 import 'package:recipes_web/pages/signup_screen/dto/sign-up.dto.dart';
 
@@ -15,7 +15,7 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     final double sw = Get.width;
-    final ApiStateController apiState = Get.put(ApiStateController());
+    // final ApiStateController apiState = Get.put(ApiStateController());
 
     return Scaffold(
       body: Container(
@@ -53,7 +53,7 @@ class SignupScreen extends StatelessWidget {
     };
 
     final AuthAPI authAPI = AuthAPI();
-    final UsersAPI usersAPI = UsersAPI();
+    // final UsersAPI usersAPI = UsersAPI();
 
     return Form(
       key: _formKey,
@@ -124,4 +124,4 @@ class SignupScreen extends StatelessWidget {
 
 }
 
-// TODO: Build signup/register screen
+// TODO(Rob): Build signup/register screen
