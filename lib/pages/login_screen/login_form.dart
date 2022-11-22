ca411d019d3d59b8cbc2165e3a5385b046785726
Rob Vandelinder <robjvan@gmail.com>
2022-11-22 09:31:55 -0330

    Refactored widget tree

---
+++ lib/pages/login_screen/login_form.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:recipes_web/api/auth_api.dart';
+import 'package:recipes_web/config/config.dart';
+import 'package:recipes_web/controllers/password_visibility.controller.dart';
+import 'package:recipes_web/pages/login_screen/dto/sign_in.dto.dart';
+import 'package:recipes_web/pages/register_screen/register_screen.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+  static final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(final BuildContext context) {
+    final TextEditingController usernameController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            //* Username (email address) field
+            _buildUsernameField(usernameController),
+            _buildPasswordField(passwordController),
+            const SizedBox(height: 32),
+            _buildLoginButton(usernameController, passwordController),
+            const SizedBox(height: 8),
+            _buildSignupButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton(
+    final TextEditingController usernameController,
+    final TextEditingController passwordController,
+  ) {
+    final AuthAPI authAPI = AuthAPI();
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(200, 60),
+        maximumSize: const Size(200, 60),
+      ),
+      child: const Text(
+        'Login',
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      onPressed: () async {
+        if (formKey.currentState!.validate()) {
+          await authAPI.signIn(
+            SignInDto(
+              username: usernameController.text,
+              password: passwordController.text,
+              platform: await Config().getPlatform(),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildSignupButton() {
+    return TextButton(
+      onPressed: () => Get.off(RegisterScreen.new),
+      style: TextButton.styleFrom(
+        minimumSize: const Size(200, 40),
+        maximumSize: const Size(200, 40),
+      ),
+      child: const Text('Register'),
+    );
+  }
+
+  Widget _buildUsernameField(final TextEditingController usernameController) {
+    return TextFormField(
+      controller: usernameController,
+      validator: (final String? value) {
+        if (value!.isEmpty) {
+          return 'Username cannot be empty';
+        } else if (!value.isEmail) {
+          return 'Username must be a valid email address';
+        }
+        return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(),
+    );
+  }
+
+  Widget _buildPasswordField(final TextEditingController passwordController) {
+    final PasswordVisibiltyController passwordVisibilty = Get.put(
+      PasswordVisibiltyController(),
+    );
+
+    return Obx(
+      () => TextFormField(
+        controller: passwordController,
+        validator: (final String? value) {
+          if (value!.isEmpty) {
+            return 'Password cannot be empty';
+          }
+          return null;
+        },
+        obscureText: passwordVisibilty.obscurePassword,
+        decoration: InputDecoration(
+          suffix: IconButton(
+            onPressed: passwordVisibilty.toggleVisibility,
+            icon: passwordVisibilty.obscurePassword
+                ? const Icon(Icons.visibility_off)
+                : const Icon(Icons.visibility),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login_screen/login_screen.dart
+++ lib/pages/login_screen/login_screen.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:recipes_web/api/auth_api.dart';
-import 'package:recipes_web/config/config.dart';
 import 'package:recipes_web/controllers/api_state.controller.dart';
-import 'package:recipes_web/pages/login_screen/dto/sign_in.dto.dart';
-import 'package:recipes_web/pages/signup_screen/signup_screen.dart';
+import 'package:recipes_web/pages/login_screen/login_form.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
   static String routeName = '/login';
-  static final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(final BuildContext context) {
@@ -29,7 +25,7 @@ class LoginScreen extends StatelessWidget {
             Obx(
               () => apiState.isLoading.value
                   ? _buildLoadingIndicator()
-                  : _buildLoginForm(),
+                  : const LoginForm(),
             ),
           ],
         ),
@@ -45,84 +41,6 @@ class LoginScreen extends StatelessWidget {
           fontWeight: FontWeight.bold,
         ),
       );
-
-  Widget _buildLoginForm() {
-    final TextEditingController usernameController = TextEditingController();
-    final TextEditingController passwordController = TextEditingController();
-    final AuthAPI authAPI = AuthAPI();
-
-    return Form(
-      key: formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            // Username (email address) field
-            TextFormField(
-              controller: usernameController,
-              validator: (final String? value) {
-                if (value!.isEmpty) {
-                  return 'Username cannot be empty';
-                }
-                return null;
-              },
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(),
-            ),
-            // Password Field
-            TextFormField(
-              controller: passwordController,
-              validator: (final String? value) {
-                if (value!.isEmpty) {
-                  return 'Password cannot be empty';
-                }
-                return null;
-              },
-              obscureText: true,
-              decoration: const InputDecoration(),
-            ),
-            const SizedBox(height: 32),
-            /// Login button
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(200, 60),
-                maximumSize: const Size(200, 60),
-              ),
-              child: const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              onPressed: () async {
-                if (formKey.currentState!.validate()) {
-                  await authAPI.signIn(
-                    SignInDto(
-                      username: usernameController.text,
-                      password: passwordController.text,
-                      platform: await Config().getPlatform(),
-                    ),
-                  );
-                }
-              },
-            ),
-            const SizedBox(height: 8),
-
-            /// Sign up button
-            TextButton(
-              onPressed: () => Get.off(SignupScreen.new),
-              style: TextButton.styleFrom(
-                minimumSize: const Size(200, 40),
-                maximumSize: const Size(200, 40),
-              ),
-              child: const Text('Register'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   Widget _buildLoadingIndicator() => const CircularProgressIndicator(
       // TODO(Rob): Add some styling to the indicator
